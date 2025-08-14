5f62bd71e7569a5c2e9d859b77c9cd415281b1b7
franfran20 <rickyegboluche@gmail.com>
2025-08-14 01:04:41 +0100

    modified genre fantasy

--- src/components/Genres.tsx
+++ src/components/Genres.tsx
@@ -3,17 +3,17 @@ import { BOOK_GENRES } from "@/utils/constants";
 export const Genres = ({ genres }: { genres: number[] }) => {
   return (
     <div className="genres">
-      {genres[0] && <span>{BOOK_GENRES[genres[0]]}</span>}
-      {genres[0] && genres[1] && (
+      {<span>{BOOK_GENRES[genres[0]]}</span>}
+      {genres[1] && (
         <div className="littleCircle" style={{ marginRight: "7px" }}></div>
       )}
 
-      {genres[1] && <span>{BOOK_GENRES[genres[1]]}</span>}
-      {genres[1] && genres[2] && (
+      {<span>{BOOK_GENRES[genres[1]]}</span>}
+      {genres[2] && (
         <div className="littleCircle" style={{ marginRight: "7px" }}></div>
       )}
 
-      {genres[2] && <span>{BOOK_GENRES[genres[2]]}</span>}
+      {<span>{BOOK_GENRES[genres[2]]}</span>}
     </div>
   );
 };
